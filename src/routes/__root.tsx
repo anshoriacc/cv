8d58136fd1d5383047e15737e86cb2337c739468
anshori <anshoriacc@gmail.com>
2026-09-22 17:58:36 +0700

    update analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,7 +35,7 @@ export const Route = createRootRoute({
       scripts: [
         {
           src: 'https://oa-c.anshori.com/oa.js',
-          'data-key': 'oa_pk_jvA_elr1dZ264DnRP4bdlXDrEHJZmw5i',
+          'data-key': 'oa_pk_LYgrMKQiSqCrS5QCz0RWMWS5h96DJ_u5',
           'data-collector': 'https://oa-c.anshori.com',
           async: true,
         },
